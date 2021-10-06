58d296aaa7f3ff7cd2e00fbbcfe93fcf8da9424a
Josekawa <zoso996@gmail.com>
2021-10-06 20:02:35 +0200

    problema con js

---
+++ Glass of water/js/app.js
@@ -0,0 +1,4 @@
+/* Busca como hacer un botón que te vaya sumando, y que el resultado lo puedas imprimir donde elijas*/
+let btn = document.createElement("button");
+btn.innerHTML = "Click Me";
+document.body.appendChild(btn);
